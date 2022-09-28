a568d98b22cbe347021aa9d1bda12ce1123e9ace
mgvictoriano <mgvictoriano@live.com>
2022-09-28 13:11:33 -0300

    Update

--- src/main/java/TelaLogin.java
+++ src/main/java/TelaLogin.java
@@ -1,2 +1,27 @@
-public class TelaLogin {
+import javax.swing.*;
+
+public class TelaLogin extends JFrame{
+    private JPanel panel1;
+    private JPasswordField passwordField1;
+    private JButton entrarButton;
+    private JTextField textField1;
+    private JTextField esqueceuSuaSenhaCliqueTextField;
+
+    public TelaLogin(String title) {
+        super(title);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setContentPane(panel1);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new TelaLogin("App Capgemini");
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
